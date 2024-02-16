e4753653a9a4e971433dfbf734cd161ac7cdb0e3
MohammedELPOP <137246353+MohammedELPOP@users.noreply.github.com>
2024-02-16 08:24:32 +0200

    Add files via upload

--- ZelzalMusic/plugins/tools/must_joinn.py
+++ ZelzalMusic/plugins/tools/must_joinn.py
@@ -5,16 +5,16 @@ from ZelzalMusic import app
 
 @app.on_message(filters.incoming & filters.private, group=-2)
 async def MUST_JOINN_channel(bot: Client, msg: Message):
-    if not "https://t.me/FLS_44":  # Not compulsory
+    if not "https://t.me/FLS_45":  # Not compulsory
         return
     try:
         try:
-            await bot.get_chat_member("FLS_44", msg.from_user.id)
+            await bot.get_chat_member("FLS_45", msg.from_user.id)
         except UserNotParticipant:
-            if "https://t.me/FLS_44".isalpha():
-                link = "https://t.me/FLS_44"
+            if "https://t.me/FLS_45".isalpha():
+                link = "https://t.me/FLS_45"
             else:
-                chat_info = await bot.get_chat("FLS_44")
+                chat_info = await bot.get_chat("FLS_45")
                 link = chat_info.invite_link
             try:
                 await msg.reply(
@@ -28,4 +28,4 @@ async def MUST_JOINN_channel(bot: Client, msg: Message):
             except ChatWriteForbidden:
                 pass
     except ChatAdminRequired:
-        print(f"I m not admin in the MUST_JOINN chat @FLS_44 !")
+        print(f"I m not admin in the MUST_JOINN chat @FLS_45 !")
