4e8497e241e58276f725df6c8d5f757932e452e4
MohammedELPOP <137246353+MohammedELPOP@users.noreply.github.com>
2024-02-16 08:08:30 +0200

    Add files via upload

---
+++ ZelzalMusic/plugins/tools/must_join.py
@@ -0,0 +1,37 @@
+from config import MUST_JOIN
+
+from pyrogram import Client, filters
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message
+from pyrogram.errors import ChatAdminRequired, UserNotParticipant, ChatWriteForbidden
+from ZelzalMusic import app
+
+
+@app.on_message(filters.incoming & filters.private, group=-1)
+async def must_join_channel(bot: Client, msg: Message):
+    if not MUST_JOIN:
+        return
+    try:
+        try:
+            await bot.get_chat_member(MUST_JOIN, msg.from_user.id)
+        except UserNotParticipant:
+            if MUST_JOIN.isalpha():
+                link = "https://t.me/" + MUST_JOIN
+            else:
+                chat_info = await bot.get_chat(MUST_JOIN)
+                link = chat_info.invite_link
+            try:
+                await msg.reply_photo(
+                    photo="https://telegra.ph/file/738661f85fe3e4fd54d20.jpg", caption=f"عليك الإنضمام إلى قناة السورس [قناة السورس]({link}) وبعدها حاول مرة اخرى !",
+                    reply_markup=InlineKeyboardMarkup(
+                        [
+                            [
+                                InlineKeyboardButton("« ᥴ𝗁ᥲᥒᥒᥱᥣ ᥉᥆υᖇᥴᥱ »", url=link),
+                            ]
+                        ]
+                    )
+                )
+                await msg.stop_propagation()
+            except ChatWriteForbidden:
+                pass
+    except ChatAdminRequired:
+        print(f"Promote me as an admin in the MUST_JOIN chat : {MUST_JOIN} !")
---
+++ ZelzalMusic/plugins/tools/must_joinn.py
@@ -0,0 +1,37 @@
+from config import MUST_JOINN
+
+from pyrogram import Client, filters
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message
+from pyrogram.errors import ChatAdminRequired, UserNotParticipant, ChatWriteForbidden
+from ZelzalMusic import app
+
+
+@app.on_message(filters.incoming & filters.private, group=-2)
+async def MUST_JOINN_channel(bot: Client, msg: Message):
+    if not MUST_JOINN:
+        return
+    try:
+        try:
+            await bot.get_chat_member(MUST_JOINN, msg.from_user.id)
+        except UserNotParticipant:
+            if MUST_JOINN.isalpha():
+                link = "https://t.me/" + MUST_JOINN
+            else:
+                chat_info = await bot.get_chat(MUST_JOINN)
+                link = chat_info.invite_link
+            try:
+                await msg.reply_photo(
+                    photo="https://telegra.ph/file/738661f85fe3e4fd54d20.jpg", caption=f"عليك الإنضمام إلى جروب السورس [جروب السورس]({link}) وبعدها حاول مرة اخرى !",
+                    reply_markup=InlineKeyboardMarkup(
+                        [
+                            [
+                                InlineKeyboardButton("« ᘜᖇ᥆υρ ᥉᥆υᖇᥴᥱ »", url=link),
+                            ]
+                        ]
+                    )
+                )
+                await msg.stop_propagation()
+            except ChatWriteForbidden:
+                pass
+    except ChatAdminRequired:
+        print(f"Promote me as an admin in the MUST_JOINN chat : {MUST_JOINN} !")
