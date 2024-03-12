a6230303167a5ada87f6bbf95a023daaf20d9834
FLASH-2 <137246353+FLASH-2@users.noreply.github.com>
2024-03-12 08:57:10 +0200

    Add files via upload

--- ZelzalMusic/plugins/play/id.py
+++ ZelzalMusic/plugins/play/id.py
@@ -14,9 +14,8 @@ from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 
 iddof = []
 @app.on_message(
-    command(["قفل الايدي","تعطيل الايدي"])
+    filters.command(["قفل الايدي","تعطيل الايدي"], "")
     & filters.group
-    & ~filters.edited
 )
 async def iddlock(client, message):
    get = await app.get_chat_member(message.chat.id, message.from_user.id)
@@ -29,9 +28,8 @@ async def iddlock(client, message):
       return await message.reply_text("لازم تكون ادمن يشخه علشان اسمع كلامك")
 
 @app.on_message(
-    command(["فتح الايدي","تفعيل الايدي"])
+    filters.command(["فتح الايدي","تفعيل الايدي"], "")
     & filters.group
-    & ~filters.edited
 )
 async def iddopen(client, message):
    get = await app.get_chat_member(message.chat.id, message.from_user.id)
@@ -48,8 +46,6 @@ async def iddopen(client, message):
 
 @app.on_message(
     command(["ايدي","id","ا"])
-    & filters.group
-    & ~filters.edited
 )
 async def iddd(client, message):
     if message.chat.id in iddof:
@@ -72,9 +68,8 @@ async def iddd(client, message):
 
 iddof = []
 @app.on_message(
-    command(["قفل جمالي","تعطيل جمالي"])
+    filters.command(["قفل جمالي","تعطيل جمالي"], "")
     & filters.group
-    & ~filters.edited
 )
 async def lllock(client, message):
    get = await app.get_chat_member(message.chat.id, message.from_user.id)
@@ -87,9 +82,8 @@ async def lllock(client, message):
       return await message.reply_text("لازم تكون ادمن يشخه علشان اسمع كلامك")
 
 @app.on_message(
-    command(["فتح جمالي","تفعيل جمالي"])
+    filters.command(["فتح جمالي","تفعيل جمالي"], "")
     & filters.group
-    & ~filters.edited
 )
 async def idljjopen(client, message):
    get = await app.get_chat_member(message.chat.id, message.from_user.id)
@@ -106,8 +100,6 @@ async def idljjopen(client, message):
 
 @app.on_message(
     command(["جمالي"])
-    & filters.group
-    & ~filters.edited
 )
 async def idjjdd(client, message):
     if message.chat.id in iddof:
